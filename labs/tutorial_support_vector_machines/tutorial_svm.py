8245bf234c4a1f4e984276fdbbfeb83f2461d345
Gerome Meyer <meyerger@students.zhaw.ch>
2022-11-02 07:01:55 +0100

    Completed SVM tutorial

--- labs/tutorial_support_vector_machines/tutorial_svm.py
+++ labs/tutorial_support_vector_machines/tutorial_svm.py
@@ -1,38 +1,71 @@
-from seaborn import load_dataset, pairplot
-from matplotlib import pyplot as plt
 import pandas as pd
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from seaborn import load_dataset, pairplot
+from sklearn.compose import make_column_transformer
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.svm import SVC
 
 df = load_dataset('penguins')
 # 11 rows have N/A values. We drop them for this tutorial.
 df = df.dropna()
-
 pairplot(df, hue='species')
 plt.show()
 
-X = df[['bill_length_mm', 'bill_depth_mm']]
+X = df.drop(columns=['species'])
 y = df['species']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=100)
 
-# SVC(
-#     C=1.0,                          # The regularization parameter
-#     kernel='rbf',                   # The kernel type used
-#     degree=3,                       # Degree of polynomial function
-#     gamma='scale',                  # The kernel coefficient
-#     coef0=0.0,                      # If kernel = 'poly'/'sigmoid'
-#     shrinking=True,                 # To use shrinking heuristic
-#     probability=False,              # Enable probability estimates
-#     tol=0.001,                      # Stopping crierion
-#     cache_size=200,                 # Size of kernel cache
-#     class_weight=None,              # The weight of each class
-#     verbose=False,                  # Enable verbose output
-#     max_iter=- 1,                   # Hard limit on iterations
-#     decision_function_shape='ovr',  # One-vs-rest or one-vs-one
-#     break_ties=False,               # How to handle breaking ties
-#     random_state=None               # Random state of the model
-# )
+column_transformer = make_column_transformer(
+    (OneHotEncoder(), ['sex', 'island']),
+    (StandardScaler(), ['bill_depth_mm', 'bill_length_mm', 'flipper_length_mm', 'body_mass_g']),
+    remainder='passthrough'
+)
 
-model = SVC(kernel='linear')
+# Transform the X columns
+X_train = column_transformer.fit_transform(X_train)
+X_train = pd.DataFrame(data=X_train, columns=column_transformer.get_feature_names_out())
+
+X_test = column_transformer.fit_transform(X_test)
+X_test = pd.DataFrame(data=X_test, columns=column_transformer.get_feature_names_out())
+
+# Run a Grid Search over the following parameters. 5x5x1 = 25
+params = {
+    'C': [0.1, 1, 10, 100, 1000],
+    'gamma': [1, .1, .01, .001, .00001],
+    'kernel': ['rbf']
+}
+
+classifier = GridSearchCV(
+    estimator=SVC(),
+    param_grid=params,
+    cv=5,  # Fitting 5 folds for each of the 25 parameter combinations = 125 folds
+    n_jobs=5,
+    verbose=1  # Configure console debug output
+)
+
+classifier.fit(X_train, y_train)
+print(classifier.best_params_)  # C: 1000, gamma: 0.01, kernel: 'rbf'
+
+model = SVC(kernel='rbf', gamma=0.01, C=1000)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+# Passing in our own data:
+penny = [{
+    'sex': 'Female',
+    'island': 'Torgersen',
+    'bill_depth_mm': 23.3,
+    'bill_length_mm': 43.5,
+    'flipper_length_mm': 190,
+    'body_mass_g': 4123
+}]
+penny = pd.DataFrame(penny)
+df_transformed = column_transformer.transform(penny)
+df_transformed = pd.DataFrame(df_transformed, columns=column_transformer.get_feature_names_out())
+predicted_species = model.predict(df_transformed)
+print(predicted_species)  # Returns: ['Adelie']
